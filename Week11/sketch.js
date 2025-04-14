83662714d6286c19414e364743892dba1ad7f398
Emily Milligan <emily.milligan@umconnect.umt.edu>
2025-04-14 11:13:04 -0600

    Add files via upload

---
+++ Week11/sketch.js
@@ -0,0 +1,105 @@
+var x = 50;
+var y = 50;
+var diameter = 25;
+var mousex = 0
+var mousey = 0
+var speed = 10
+var ellipsex = 375
+var ellipsey = 100
+var rectx = 100
+var recty = 300
+
+
+function setup() {
+  createCanvas(500,400);
+}
+
+function draw() {
+  background(500);
+  fill(20,20,100)
+  rect(490,0,30,400)
+  fill(138,43,226)
+  ellipsex += random(-speed,speed);
+  ellipsey += random(-speed,speed);
+  ellipse(ellipsex,ellipsey,25,60)
+  if(ellipsex >= 500)
+    {
+      ellipsex = 0
+    }
+  if(ellipsey >= 400)
+    {
+      ellipsey = 0
+    }
+  if(ellipsex <= 0)
+    {
+      ellipsex = 500
+    }
+  if(ellipsey <= 0)
+    {
+      ellipsey = 400
+    }
+  rectx += random(-speed,speed);
+  recty += random(-speed,speed);
+  fill(220,20,60)
+  rect(rectx,recty,50,75)
+  if(rectx >= 500)
+    {
+      rectx = 0
+    }
+  if(recty >= 400)
+    {
+      recty = 0
+    }
+  if(rectx <= 0)
+    {
+      rectx = 500
+    }
+  if(recty <= 0)
+    {
+      recty = 400
+    }
+  
+  fill(20,20,100)
+  circle(x,y,diameter)
+   if (x >= 600) {
+    x = 50;
+  }
+  if (keyIsDown(83)) {
+    y += 5;
+  } else if (keyIsDown(87)) {
+    y -= 5;
+  }
+
+  if (y >= 400) {
+    y = 50;
+  }
+  if (x >= 600) {
+    x = 50;
+  }
+  if (keyIsDown(68)) {
+    x += 5;
+  } else if (keyIsDown(65)) {
+    x -= 5;
+  }
+
+  if (y >= 400) {
+    y = 50;
+    
+  }
+  
+  
+  if (x >= 500){
+     fill(0);
+        stroke(5);
+        textSize(26);
+        text("You Win!", width/2-50, height/2-50);
+  }
+fill(255,255,0)
+  square(mousex,mousey, 50)
+}
+function mouseClicked()
+{
+  mousex = mouseX;
+  mousey = mouseY;
+}
+ 
